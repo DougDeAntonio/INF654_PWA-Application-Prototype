bb37b63ee86e9c84c02ed8f78663ec9a1e74a96c
ddeantonio <dcdeantonio@mail.fhsu.edu>
2025-09-27 15:14:27 -0400

    Initial commit with project files

---
+++ js/app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const addButton = document.querySelector('.btn-floating');
+    addButton.addEventListener('click', function() {
+        console.log('Add task clicked');
+    });
+
+});
